c1c87c3c548182b0d336d5416958591c8e648c8a
Priyanshu Gour <priyanshugour1@gmail.com>
2024-07-20 23:17:44 +0530

    INIT

---
+++ src/main/java/com/infinite/titan/TitanApplication.java
@@ -0,0 +1,13 @@
+package com.infinite.titan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TitanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TitanApplication.class, args);
+	}
+
+}
